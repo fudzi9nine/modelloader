19416cc3fbd0fa4f6b4888add72bcba9fb5b49e2
Alex.Hlivitski <aliaksandr.hlivitski@softensity.com>
2024-01-22 12:07:29 +0300

    show dataUrl

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import * as THREE from "three";
 import { Canvas, useLoader } from "@react-three/fiber";
-import { OrbitControls, Shadow, Center, Resize, PerspectiveCamera, CameraControls, Plane } from "@react-three/drei";
+import { OrbitControls, PerspectiveCamera, CameraControls } from "@react-three/drei";
 import { OBJLoader } from "three/examples/jsm/loaders/OBJLoader";
 import { Suspense, useEffect } from "react";
 import { Mesh, Vector3 } from "three";
@@ -18,9 +18,8 @@ const material = new THREE.MeshPhongMaterial({
 const Scene = () => {
   const searchParams = new URLSearchParams(document.location.search)
 
-  const url = searchParams.get('dataUrl')
-  console.log('here: ', url)
-  const obj = useLoader(OBJLoader, 'https://tabeebprod.blob.core.windows.net/assets/SX-zdhk-ypy8eANSGJUq2_teapot.obj?skoid=41953f27-736e-4824-aa75-c298dd636fe7&sktid=78640a62-55b2-4751-97e3-f421dfe38721&skt=2024-01-21T13%3A55%3A03Z&ske=2024-01-28T13%3A55%3A03Z&sks=b&skv=2021-06-08&sv=2021-06-08&st=2024-01-22T08%3A08%3A33Z&se=2024-01-22T13%3A12%3A33Z&sr=b&sp=r&sig=kMhtFSAIDgE4zZAsiVj6%2Fy27913Sx7RV5bmIYillqc8%3D')
+  const dataUrl = searchParams.get('dataUrl')
+  const obj = useLoader(OBJLoader, dataUrl)
 
   useEffect(() => {
     if(obj){
